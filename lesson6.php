eab05092e114b70b79203cfd62c9e489056c99ae
Pimpys <maxkyivua@gmail.com>
2018-12-01 01:07:10 +0200

    lesson6

---
+++ lesson6.php
@@ -0,0 +1,40 @@
+<?php
+
+//$array = array();
+//$array = [];
+$array[] = 10; //$array[0] = 10;
+$array['ten'] = 10;
+$array['assoc'] = [
+    100 => 1,
+    'ten' => 10,
+    TRUE,
+    FALSE,
+    null,
+    'strin type',
+];
+echo '<pre>';
+//var_dump($array);
+print_r($array);
+//$count = count($array['assoc']);
+//
+//print $count;
+
+/*
+ * Задание 1
+1. Создайте массив bmw с ячейками:
+"model"
+"speed"
+"doors"
+"year"
+2. Заполните ячейки значениями:
+"X5", 120, 5, "2006"
+3. Создайте массивы toyota и opel аналогичные массиву bmw.
+4. Заполните массив toyota значениями:
+"Carina", 130, 4, "2007"
+5. Заполните массив opel значениями:
+"Corsa", 140, 5, "2007"
+Задание 2
+Выведите значения всех трёх массивов в виде:
+name - model - speed - doors - year, например:
+bmw - 120 - 5 – 2006
+ */
